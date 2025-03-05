a103b3649f4e4cca8ea3c7a219bbcfae3b0714e3
Тон <id0994597549@gmail.com>
2025-03-05 19:13:26 +0100

    finish work a 6 task

--- js/task-1.js
+++ js/task-1.js
@@ -1,30 +1,17 @@
-const customer = {
-  username: "Mango",
-  balance: 24000,
-  discount: 0.1,
-  orders: ["Burger", "Pizza", "Salad"],
-  // Change code below this line
-  getBalance() {
-    return this.balance;
-  },
-  getDiscount() {
-    return this.discount;
-  },
-  setDiscount(value) {
-    this.discount = value;
-  },
-  getOrders() {
-    return this.orders;
-  },
-  addOrder(cost, order) {
-    this.balance -= cost - cost * this.discount;
-    this.orders.push(order);
-  },
-  // Change code above this line
-};
+const categoriesList = document.querySelectorAll(".categories-item");
+const elementsOfCategory = document.querySelectorAll(
+  ".elements-list > .elem-list-items"
+);
 
-customer.setDiscount(0.15);
-console.log(customer.getDiscount()); // 0.15
-customer.addOrder(5000, "Steak");
-console.log(customer.getBalance()); // 19750
-console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
+console.log(`Number of categories: ${categoriesList.length}`);
+
+categoriesList.forEach((category) => {
+  const nameOfCategory = category.querySelector(".item-title").textContent;
+
+  const elementsOfCategory = category.querySelectorAll(
+    ".elements-list > .elem-list-items"
+  ).length;
+
+  console.log(`Category: ${nameOfCategory}`);
+  console.log(`Elements: ${elementsOfCategory}`);
+});
--- js/task-3.js
+++ js/task-3.js
@@ -1,33 +1,10 @@
-class StringBuilder {
-  #value;
+const yourName = document.querySelector("#name-input");
+const nameOut = document.querySelector("#name-output");
+yourName.addEventListener("input", (event) => {
+  const inputValue = event.currentTarget.value;
+  const trimmedValue = inputValue.trim();
 
-  constructor(initialValue) {
-    this.#value = initialValue;
-  }
-
-  getValue() {
-    return this.#value;
-  }
-
-  padEnd(str) {
-    this.#value += str;
-  }
-
-  padStart(str) {
-    this.#value = str + this.#value;
-  }
-
-  padBoth(str) {
-    this.padStart(str);
-    this.padEnd(str);
-  }
-}
-
-const builder = new StringBuilder(".");
-console.log(builder.getValue()); // "."
-builder.padStart("^");
-console.log(builder.getValue()); // "^."
-builder.padEnd("^");
-console.log(builder.getValue()); // "^.^"
-builder.padBoth("=");
-console.log(builder.getValue()); // "=^.^="
+  nameOut.textContent = trimmedValue
+    ? (nameOut.textContent = trimmedValue)
+    : (nameOut.textContent = `Anonymus`);
+});
